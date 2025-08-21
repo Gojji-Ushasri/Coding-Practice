dd651bd26707897e1fdfbfe61a0410251b333947
Vaishnavi-Parsa <vaishnaviparsa@gmail.com>
2025-08-21 21:21:23 +0530

    Initialise commit

--- PassingArgumentsFromTerminal.java
+++ PassingArgumentsFromTerminal.java
@@ -1,7 +1,7 @@
 class PassingArgumentFromTerminal{
     public static void main(String[] args){
         System.out.print(args[0]);
-        System.out.print(" "+Congratulates+" ");
+        System.out.print(" "+"Congratulates"+" ");
         System.out.print(args[1]);
     }
 }
